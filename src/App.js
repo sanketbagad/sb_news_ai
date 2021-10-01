47bb9a45c079de2b40cdd81f7f7f49e0d85feb7d
Sanket Bagad <alencolins@gmail.com>
2021-10-01 10:46:48 +0530

    Initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            
+        })
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
