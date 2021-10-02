c2101383562894443257c64b5de69c2a8cdd834f
Sanket Bagad <alencolins@gmail.com>
2021-10-02 16:12:22 +0530

    Adding modal and modifying the code

--- src/Components/NewsCards/NewsCards.js
+++ src/Components/NewsCards/NewsCards.js
@@ -8,11 +8,11 @@ const NewsCards = ({ articles }) => {
     return (
         <Grow in>
             <Grid className={classes.container} container alignItems="stretch">
-            {articles.map((article, i) => {
-                <Grid item xs={12} sm={6} md={6} lg={3} style={{display: 'flex'}}>
-                <NewsCard article={article} i={i} />
-                </Grid>
-            })}
+            {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <NewsCard i={i} article={article} />
+          </Grid>
+        ))}
             </Grid>
         </Grow>
     )
