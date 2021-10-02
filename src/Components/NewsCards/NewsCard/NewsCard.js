1ec455e9eb7f216704a2af16a01b4b77be1a3c95
Sanket Bagad <alencolins@gmail.com>
2021-10-02 16:05:44 +0530

    Setting up the styles

--- src/Components/NewsCards/NewsCard/NewsCard.js
+++ src/Components/NewsCards/NewsCard/NewsCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
-
+import useStyles from "./styles"
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i}) => {
+   
+    const classes = useStyles()
     return (
         <Card>
             <CardActionArea>
-                <CardMedia image={urlToImage} />
+                <CardMedia className={classes.media} image={urlToImage} />
                 <div>
                     <Typography variant="body2" color="textSecondary" component="h2">
                         {(new Date(publishedAt)).toDateString()}
